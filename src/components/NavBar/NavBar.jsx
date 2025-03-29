cec0d7e2b66d84335e9d502bfad0b46a3f133ab0
Carlos Eduardo B. Kessler <kadukessler@gmail.com>
2025-03-28 21:12:52 -0300

    ✨ feat: reformular a barra de navegação utilizando MUI para melhor responsividade e estética

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,23 +1,49 @@
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/sobre">Sobre</Link>
-        </li>
-        <li>
-          <Link to="/livros">Lista de livros</Link>
-        </li>
-        <li>
-          <Link to="/cadastrar">Cadastrar</Link>
-        </li>
-      </ul>
-    </nav>
+    <AppBar
+      position="static"
+      elevation={0}
+      sx={{ background: "transparent", backdropFilter: "blur(6px)" }}
+    >
+      <Container maxWidth="lg">
+        <Toolbar disableGutters sx={{ justifyContent: "space-between" }}>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, fontWeight: 600 }}
+          >
+            📚 Reading Journal
+          </Typography>
+
+          {/* LINKS CENTRAIS */}
+          <Box sx={{ display: "flex", gap: 3, mx: "auto" }}>
+            <Button component={Link} to="/" color="inherit">
+              Home
+            </Button>
+            <Button component={Link} to="/sobre" color="inherit">
+              Sobre
+            </Button>
+            <Button component={Link} to="/livros" color="inherit">
+              Lista de livros
+            </Button>
+            <Button component={Link} to="/cadastrar" color="inherit">
+              Cadastrar
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
 
